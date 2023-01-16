03c3853e6697f9c0963f5c4a5770de13457574b5
ofek1 <ofekyaloz16@gmail.com>
2023-01-16 20:44:50 +0200

    comments

--- lib/search.dart
+++ lib/search.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:http/http.dart';
 import 'utils.dart';
-import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'searchGameName.dart';
 
