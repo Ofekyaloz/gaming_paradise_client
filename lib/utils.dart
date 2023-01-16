aea5002ffe8ae1ffaf101b51a7e4400b96418bb4
ofek1 <ofekyaloz16@gmail.com>
2023-01-16 20:59:06 +0200

    comments

--- lib/utils.dart
+++ lib/utils.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/foundation.dart' show kIsWeb;
 
 class Constants {
+  // web url is localhost, device 10.0.2.2
   static String url =
       kIsWeb ? 'http://localhost:8000/api/' : 'http://10.0.2.2:8000/api/';
 
